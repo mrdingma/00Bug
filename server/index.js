729375d9739bee7e3ba20b77ff8a0ece2fa2f9e2
Dean Ma <mrdingma@gmail.com>
2020-02-29 14:28:54 -0800

    setup routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
-// Import the express framework for our node server
 const express = require('express');
-// Import the path module from node to create absolute file paths for express static
 const path = require('path');
+const routeHandler = require('./routes');
 
-// Instantiate the express server
 const app = express();
-// Set a constant for the port that our express server will listen on
-const PORT = 3000;
 
-// Serve static files. Any requests for specific files will be served if they exist in the provided folder
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
-// Start the server on the provided port
+app.get('/favicon.ico', (req, res) => {
+  res.status(204);
+});
+app.get('/robots.txt', (req, res) => {
+  res.status(204);
+});
+
+app.use('/', routeHandler());
+
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
