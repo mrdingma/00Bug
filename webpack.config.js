bbe1f70ae533873b4b953f9cabb4e0b69e2e31e8
Dean Ma <mrdingma@gmail.com>
2020-02-29 19:01:40 -0800

    configure css

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
-  
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      } 
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
