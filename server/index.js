e3dc215ab641e78838d03fb214ecdf09e0594072
Dean Ma <mrdingma@gmail.com>
2020-03-02 15:08:38 -0800

    add morgan

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const routeHandler = require('./routes');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('/favicon.ico', (req, res) => {
   res.status(204);
