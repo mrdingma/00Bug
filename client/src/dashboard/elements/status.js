4f6640db809c1897958a56e75e6d82c48397c73d
Dean Ma <mrdingma@gmail.com>
2020-03-02 15:07:11 -0800

    styling

--- client/src/dashboard/elements/status.js
+++ client/src/dashboard/elements/status.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const status = styled.span`
   padding: 1px 5px !important;
   font-weight: 300 !important;
-  margin-left: 30%;
   background-color: #ed8077;
   border-radius: 20px;
   ${(props) => (props.color ? `background-color: ${props.color};` : null)};
