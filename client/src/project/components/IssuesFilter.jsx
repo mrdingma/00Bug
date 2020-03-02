c043c11ef0094baedf722c54d66f0298301b4e22
Dean Ma <mrdingma@gmail.com>
2020-03-02 10:21:14 -0800

    init issues listing component

---
+++ client/src/project/components/IssuesFilter.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import RecentUpdatesList from './RecentUpdatesList.jsx';
+
+const IssuesFilter = (props) => {
+  // const [isClicked, setIsClicked] = useState(false);
+
+  // const onClickHandler = () => {
+  //   setIsClicked(!isClicked);
+  // };
+
+
+  let content = (
+
+  );
+
+  return content;
+};
+
+export default IssuesFilter;
