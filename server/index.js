57b98f6c4b5f2cc4af4ff0851014c3c00e1d525b
Dean Ma <mrdingma@gmail.com>
2020-03-02 21:18:59 -0800

    fix typo

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const routeHandler = require('./routes');
 const morgan = require('morgan');
-
-const app = express();
+const mongoose = require('mongoose');
+const config = require('./config');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
+
+// set up db connection
+mongoose.connect(config.mongodb.dsn, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.get('/favicon.ico', (req, res) => {
   res.status(204);
 });
+
 app.get('/robots.txt', (req, res) => {
   res.status(204);
 });
