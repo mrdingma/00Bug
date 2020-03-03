275e18c570fe998af3dc86859b009c31c62b6e08
Dean Ma <mrdingma@gmail.com>
2020-03-02 21:17:56 -0800

    init controllers

---
+++ server/controllers/project.js
@@ -0,0 +1,15 @@
+const ProjectModel = require('../models/Project');
+
+async function addNew(data) {
+  const project = new ProjectModel(data);
+  return project.save();
+};
+
+async function getAllByUser(userId) {
+  return ProjectModel.find({ userId });
+};
+
+module.exports = {
+  addNew,
+  getAllByUser,
+};
