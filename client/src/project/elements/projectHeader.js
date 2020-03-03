b4798c18dae7d17e54fd01dfed910f8a354cf5bb
Dean Ma <mrdingma@gmail.com>
2020-03-03 12:19:12 -0800

    move header to middle

--- client/src/project/elements/projectHeader.js
+++ client/src/project/elements/projectHeader.js
@@ -4,7 +4,7 @@ const ProjectHeader = styled.div`
   position: absolute;
   width: 100%;
   border: 1px solid grey;
-  padding-left: 8%;
+  padding-left: 50%;
   padding-top: 10px;
   padding-bottom: 17px;
 }
