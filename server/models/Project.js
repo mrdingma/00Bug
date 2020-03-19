b2dd0542ca8a18858ef12bd31c1c3d6e3ca764bf
Dean Ma <mrdingma@gmail.com>
2020-03-18 23:32:12 -0700

    change project schema from int ID to string ID

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const ProjectSchema = new Schema({
-  userId: Number,
-  name: String,
+  userId: String,
+  name: String
 });
 
-module.exports = mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model("Project", ProjectSchema);
