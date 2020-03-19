18bb062aa53312670f84dd6ef3012c109b6c26a2
Dean Ma <mrdingma@gmail.com>
2020-03-18 23:32:49 -0700

    no change

--- server/routes/issues/index.js
+++ server/routes/issues/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const issue_controller = require('../../controllers/issue');
+const express = require("express");
+const issue_controller = require("../../controllers/issue");
 
 module.exports = () => {
   const router = express.Router();
 
   // GET issues by userId
-  router.get('/user/:userid', async (req, res, next) => {
+  router.get("/user/:userid", async (req, res, next) => {
     try {
       const issues = await issue_controller.getAllByUser(req.params.userid);
       return res.send(issues);
@@ -15,16 +15,19 @@ module.exports = () => {
   });
 
   // GET issues by userId and projectId
-  router.get('/user/:userid/project/:projectName', async (req, res, next) => {
+  router.get("/user/:userid/project/:projectName", async (req, res, next) => {
     try {
-      const issues = await issue_controller.getAllByUserAndProject(Number(req.params.userid), req.params.projectName);
+      const issues = await issue_controller.getAllByUserAndProject(
+        req.params.userid,
+        req.params.projectName
+      );
       return res.send(issues);
     } catch (err) {
       return next(err);
     }
   });
 
-  router.post('/', async (req, res, next) => {
+  router.post("/", async (req, res, next) => {
     try {
       const issue = await issue_controller.addNew(req.body);
       return res.send(issue);
@@ -33,7 +36,7 @@ module.exports = () => {
     }
   });
 
-  router.delete('/:issueid', async (req, res, next) => {
+  router.delete("/:issueid", async (req, res, next) => {
     try {
       const deleteIssue = await issue_controller.deleteOne(req.params.issueid);
       return res.sendStatus(200);
@@ -42,7 +45,7 @@ module.exports = () => {
     }
   });
 
-  router.put('/:issueid', async (req, res, next) => {
+  router.put("/:issueid", async (req, res, next) => {
     try {
       const updateIssue = await issue_controller.update(req.body);
       return res.send(200);
