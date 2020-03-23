44535eaa8a56048ddcb5662720dafa3e2bc08676
Dean Ma <mrdingma@gmail.com>
2020-03-23 13:45:02 -0700

    add color for closed status

--- client/src/project/components/IssueStatusTag.jsx
+++ client/src/project/components/IssueStatusTag.jsx
@@ -5,7 +5,8 @@ const IssueStatusTag = ({ issue }) => {
   const statusColorMapper = {
     open: "#ED8077",
     in_progress: "#4488C5",
-    resolved: "#5EB5A6"
+    resolved: "#5EB5A6",
+    closed: "#B0BE3C"
   };
 
   const map = {
