92968562b24e045608a85ccb46420643ef40d627
Dean Ma <mrdingma@gmail.com>
2020-03-24 21:19:04 -0700

    add loader & email verification screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "./auth0.jsx";
 import Home from "./Home.jsx";
+import VerifyEmail from "./VerifyEmail.jsx";
 
 function App() {
   useEffect(() => {}, []);
@@ -8,14 +9,28 @@ function App() {
   const { loading, user, loginWithRedirect } = useAuth0();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div style={{ left: "50%" }} class="preloader-wrapper big active">
+        <div class="spinner-layer spinner-green-only">
+          <div class="circle-clipper left">
+            <div class="circle"></div>
+          </div>
+          <div class="gap-patch">
+            <div class="circle"></div>
+          </div>
+          <div class="circle-clipper right">
+            <div class="circle"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="App">
       {!user && loginWithRedirect({})}
-
-      {user && <Home />}
+      {user && !user.email_verified && <VerifyEmail />}
+      {user && user.email_verified && <Home />}
     </div>
   );
 }
