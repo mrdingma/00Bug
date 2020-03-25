a913d4004dca8652834789eb75dedb39f40dd3b5
Dean Ma <mrdingma@gmail.com>
2020-03-24 21:23:59 -0700

    autofill assignees & assignee chips creation

--- client/src/project/components/NewIssueForm.jsx
+++ client/src/project/components/NewIssueForm.jsx
@@ -3,14 +3,16 @@ import { DatePicker } from "react-materialize";
 import FormBody from "../elements/formBody";
 import date from "date-and-time";
 import { useAuth0 } from "../../auth0.jsx";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const NewIssueForm = ({ currentProject, addIssue, friends }) => {
+const NewIssueForm = ({ currentProject, addIssue, friends, addProject }) => {
   const [subject, setSubject] = useState("");
   const [type, setType] = useState("task");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("");
   const [dueDate, setDueDate] = useState("");
-  const [assignee, setAssignee] = useState("");
+  const [assigneeText, setAssigneeText] = useState("");
+  const [assignee, setAssignee] = useState([]);
   const [attachment, setAttachment] = useState("");
   const [confirmScreen, setConfirmScreen] = useState(false);
 
@@ -30,17 +32,22 @@ const NewIssueForm = ({ currentProject, addIssue, friends }) => {
     ) {
       setAttachment(file);
     } else {
-      alert("Only acceptable formats are JPG or PNG format");
+      M.toast({
+        html: "Only acceptable formats are JPG or PNG format",
+        inDuration: 100,
+        outDuration: 100
+      });
     }
   };
 
-  const clickHandler = () => {
+  const clickAddIssueHandler = () => {
     const payload = new FormData();
     payload.append("image", attachment);
     payload.set("userId", user.name);
     payload.set("project", currentProject);
     payload.set("status", "open");
     payload.set("due_date", dueDate);
+    payload.set("picture", user.picture);
     payload.set("summary", subject);
     payload.set("priority", priority);
     payload.set("type", type);
@@ -48,9 +55,86 @@ const NewIssueForm = ({ currentProject, addIssue, friends }) => {
     payload.set("assignee", assignee);
     payload.set("assigner", user.name);
 
+    // add project to each assignee
+    assignee.forEach(email => addProject(email, currentProject));
+
     addIssue(payload);
     setAttachment("");
     setConfirmScreen(true);
+  };
+
+  const isEmail = email => {
+    return /[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/.test(email);
+  };
+
+  const isInList = email => {
+    return assignee.includes(email);
+  };
+
+  const isValid = email => {
+    if (isInList(email)) {
+      return false;
+    }
+
+    if (!isEmail(email)) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const createChip = text => {
+    if (text && isValid(text)) {
+      setAssignee([...assignee, text]);
+      setAssigneeText("");
+    } else if (isInList(text)) {
+      M.toast({
+        html: "Already added",
+        inDuration: 100,
+        outDuration: 100
+      });
+    } else {
+      M.toast({
+        html: "Invalid email format",
+        inDuration: 100,
+        outDuration: 100
+      });
+    }
+  };
+
+  const handleKeyDown = e => {
+    if (["Enter", "Tab", ","].includes(e.key)) {
+      e.preventDefault();
+      var value = assigneeText.trim();
+
+      createChip(value);
+    }
+  };
+
+  const handleChange = e => {
+    setAssigneeText(e.target.value);
+  };
+
+  const handleDelete = email => {
+    setAssignee(assignee.filter(text => text !== email));
+  };
+
+  const handlePaste = e => {
+    e.preventDefault();
+
+    var paste = e.clipboardData.getData("text");
+    var emails = paste.match(/[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/g);
+
+    if (emails) {
+      var toBeAdded = emails.filter(email => !isInList(email));
+
+      setAssignee([...assignee, ...toBeAdded]);
+    }
+  };
+
+  const autoCompleteHandler = e => {
+    createChip(e.target.innerText);
+    setAssigneeText("");
   };
 
   useEffect(() => {
@@ -59,16 +143,16 @@ const NewIssueForm = ({ currentProject, addIssue, friends }) => {
     M.textareaAutoResize($("#textarea1"));
   });
 
-  useEffect(() => {
-    setAssignee(user.name);
-  }, []);
+  // useEffect(() => {
+  //   setAssignee(user.name);
+  // }, []);
 
   let content = (
     <>
       <div className="row">
         <form className="col s12">
           <div name="type" className="row">
-            <div className="input-field col s2">
+            <div className="col s2">
               <select
                 className="selectTask"
                 onChange={e => setType(e.target.value)}
@@ -122,15 +206,40 @@ const NewIssueForm = ({ currentProject, addIssue, friends }) => {
               >
                 <div>Assignee</div>
                 <div style={{ paddingLeft: "24px", width: "100%" }}>
-                  <select
-                    className="selectTask"
-                    onChange={e => setAssignee(e.target.value)}
-                  >
-                    <option value={user.name}>Myself</option>
-                    {friends.map(friend => (
-                      <option value={friend}>{friend}</option>
-                    ))}
-                  </select>
+                  {assignee.map(item => (
+                    <div className="tag-item" key={item}>
+                      {item}
+                      <button
+                        type="button"
+                        className="button"
+                        onClick={() => handleDelete(item)}
+                      >
+                        &times;
+                      </button>
+                    </div>
+                  ))}
+                  <input
+                    className="input"
+                    value={assigneeText}
+                    placeholder="Type email(s) and press Enter"
+                    onKeyDown={e => handleKeyDown(e)}
+                    onChange={e => handleChange(e)}
+                    onPaste={e => handlePaste(e)}
+                  />
+                  {assigneeText.length > 0 && (
+                    <ul className="autocomplete-ul">
+                      {friends
+                        .filter(friend => friend.includes(assigneeText))
+                        .map(friend => (
+                          <li
+                            className="autocomplete-li"
+                            onClick={autoCompleteHandler}
+                          >
+                            <span>{friend}</span>
+                          </li>
+                        ))}
+                    </ul>
+                  )}
                 </div>
               </div>
               <div
@@ -201,7 +310,7 @@ const NewIssueForm = ({ currentProject, addIssue, friends }) => {
                 className="col s5 offset-s2"
                 style={{ paddingLeft: "17%", paddingTop: "3%" }}
               >
-                <a class="waves-effect btn" onClick={clickHandler}>
+                <a class="waves-effect btn" onClick={clickAddIssueHandler}>
                   Add
                 </a>
               </div>
